7f2658629d9dc706dd78deaa4c8d4953c1e4f576
Seitek2002 <seitek_2002@bk.ru>
2022-03-28 15:36:25 +0600

    testa

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.static(__dirname));
 app.use(express.static(path.resolve(__dirname, "build")));
 
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+})
+
 app.listen(PORT, () => {
     console.log("listen on port", PORT);
 });
